5e43b65be82f928e0c4a6e043befe4d069cf3302
Isaiah <148330546+DatsIsaiah@users.noreply.github.com>
2023-11-02 23:25:54 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "ucm_utils.h"
+using namespace std;
+
+int main() {
+
+string word;
+getline(cin, word);
+
+    cout << funkyString(word) << endl;
+
+
+
+    return 0;
+}
